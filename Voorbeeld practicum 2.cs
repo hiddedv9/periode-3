47dbf0a1228c5ee12f868f84731eb740e5085db0
Hidde <hiddedv9@gmail.com>
2018-02-13 16:07:15 +0100

    opdr 2 klaar

---
+++ Voorbeeld practicum 2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GevorderdProgrammerenPracticumWeek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Villain ozymandias = new Villain("Adrian Veidt", 60);
+            Villain joker = new Villain("Mr. J", 70);
+            Person robin = new Hero("Jason Todd", joker, 30);
+            Hero batman = new Hero("Bruce Wayne", joker, robin, 40);
+            batman.AddStrength("Intelligence");
+            batman.AddStrength("Wealth");
+            Person rorschach = new Hero("Walter Joseph Kovacs", ozymandias, 50);
+            Villain gru = new Villain("Gru", 90);
+            Minion bob = new Minion("Bob", "Dumb");
+            Minion kevin = new Minion("Kevin", "Dumb");
+            Minion stuart = new Minion("Stuart", "Dumb");
+            gru.AddMinion(bob);
+            gru.AddMinion(kevin);
+            gru.AddMinion(stuart);
+
+            Console.WriteLine("BATMAN");
+            Console.WriteLine(batman.GetName());
+            foreach(string strenght in batman.GetStrengths())
+            {
+                Console.WriteLine(strenght);
+            }
+            Console.WriteLine(batman.GetArchnemesis().GetName());
+            Console.WriteLine(batman.GetArchnemesis().GetEvilness());
+
+            Console.WriteLine("GRU");
+            Console.WriteLine(gru.GetName());
+            foreach (Minion minion in gru.GetMinions())
+            {
+                Console.WriteLine(minion.GetName());
+                Console.WriteLine(minion.GetEvilness());
+                Console.WriteLine(minion.GetWeakness());
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- opdr2/opdr2/Hero.cs
+++ opdr2/opdr2/Hero.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace opdr2
 {
-    class Hero
+    class Hero : Person, IheeftKrachten
     {
+        /// <summary>
+        /// Variabelen
+        /// </summary>
+        private Villain archnemesis;
+        private List<string> krachten = new List<string>();
+        private Person sidekick;
+
+        public Hero(string name, Villain nemesis, Person sidekick, int evilness)
+        {
+            this.name = name;
+            this.archnemesis = nemesis;
+            this.sidekick = sidekick;
+            if (evilness > 100)
+            {
+                this.evilness = 50;
+            }
+            else
+                 this.evilness = evilness;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+        public int getEvilness()
+        {
+            return this.evilness;
+        }
+        public void updateSidekick(Person sidekick)
+        {
+            this.sidekick = sidekick;
+        }
+
+        public Person getSidekick()
+        {
+            return this.sidekick;
+        }
+
+        public Villain getArchnemesis()
+        {
+            return this.archnemesis;
+        }
+
+        public void addStrength(string kracht)
+        {
+            krachten.Add(kracht);
+        }
+
+        public List<string> getKrachten()
+        {
+            return this.krachten;
+        }
     }
 }
